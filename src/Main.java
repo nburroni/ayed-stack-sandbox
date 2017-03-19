27d628ec15bd6da76c027a5e2d4da9f7df1a2cc0
Nicolas Burroni <nicolas.burroni@ing.austral.edu.ar>
2017-03-19 16:23:16 -0300

    First commit. Lib with jar, basic Main class and gitignore

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import ua.ayed.stack.Stack;
+import ua.ayed.stack.StackAYED;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Stack<Character> stack = new StackAYED<>();
+    }
+
+}
